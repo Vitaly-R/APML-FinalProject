420bbfb654a3ac60a61dd14d42660a953ffb5eff
Ziv Mahluf <ziv5467@gmail.com>
2020-03-04 23:54:05 +0200

    changed epsilon calculation update to train happens every learn (instead of every 50) batch threshold is much lower (100) bigger deque (500) t=0.5

--- policies/custom.py
+++ policies/custom.py
@@ -8,18 +8,19 @@ from collections import deque
 import numpy as np
 import random as r
 
-
 VALUES = 11
 LEARNING_RATE = 1e-3
 BATCH_SIZE = 15
-MAX_BATCH_SIZE = 10
+MAX_BATCH_SIZE = 30
 MIN_BATCH_SIZE = 1
-BATCH_THRESHOLD = 20 * BATCH_SIZE
+# BATCH_THRESHOLD = 20 * BATCH_SIZE
+BATCH_THRESHOLD = 100
 RADIUS = 5
 GAMMA = 0.15
-INITIAL_EPSILON = 1.0
-EPSILON_DECAY = 0.999
-EPSILON_MIN = 0.01
+INITIAL_EPSILON = 0.6
+# EPSILON_DECAY = 0.999
+EPSILON_DECAY_ROUND = BATCH_THRESHOLD
+# EPSILON_MIN = 0.05
 WINDOW_SIDE_LENGTH = 2 * RADIUS + 1
 NUM_ELEMENTS = WINDOW_SIDE_LENGTH ** 2
 MODEL = 0  # An indicator of weather to use a convolutional model, used solely for testing
@@ -46,6 +47,9 @@ class CustomPolicy(Policy):
         to load your pickled model and set the variables accordingly, if the
         game uses a saved model and is not a training session.
         """
+        self.epsilon = self.__dict__['epsilon']
+        self.lr = self.__dict__['lr']
+        self.gamma = self.__dict__['gamma']
         if self.__dict__['model'] == 0:
             print('creating dense network')
             self.model = self.create_model_1()
@@ -58,18 +62,22 @@ class CustomPolicy(Policy):
             self.target_model = self.create_model_2()
             self.model.predict(np.zeros((1, WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH, 1)))
             self.__process_state = self.__process_state_2
-        else:
+        elif self.__dict__['model'] == 2:
             print('creating dense network')
             self.model = self.create_model_4()
             self.target_model = self.create_model_4()
             self.model.predict(np.zeros((1, NUM_ELEMENTS)))
             self.__process_state = self.__process_state_4
-        self.memory = deque(maxlen=BATCH_THRESHOLD * 2)
+        else:
+            print('creating simple network')
+            self.model = self.create_model_3()
+            self.target_model = self.create_model_3()
+            self.model.predict(np.zeros((1, NUM_ELEMENTS)))
+            self.__process_state = self.__process_state_4
+        self.exploration_decay = self.epsilon / (self.game_duration - self.score_scope - EPSILON_DECAY_ROUND)
+        self.memory = deque(maxlen=BATCH_THRESHOLD * 5)
         self.batch_size = BATCH_SIZE
-        self.t = 0.125  # a learning rate for the weights of the target model in relation to the main model
-        self.epsilon = self.__dict__['epsilon']
-        self.lr = self.__dict__['lr']
-        self.gamma = self.__dict__['gamma']
+        self.t = 0.5  # a learning rate for the weights of the target model in relation to the main model
 
     def learn(self, round, prev_state, prev_action, reward, new_state, too_slow):
         """
@@ -87,9 +95,11 @@ class CustomPolicy(Policy):
                         policy for a few rounds in a row. you may use this to make your
                         computation time smaller (by lowering the batch size for example).
         """
-        if round > BATCH_THRESHOLD:
-            self.epsilon = self.epsilon if self.epsilon <= EPSILON_MIN else self.epsilon * EPSILON_DECAY
+        if round >= EPSILON_DECAY_ROUND:
+            # self.epsilon = EPSILON_MIN if self.epsilon <= EPSILON_MIN else self.epsilon - self.exploration_decay
+            self.epsilon = self.epsilon - self.exploration_decay
 
+        if round > BATCH_THRESHOLD:
             # # In order for the model not to start training too early, we wait until enough states are saved.
             # if too_slow:
             #     self.batch_size = max(self.batch_size // 2, MIN_BATCH_SIZE)
@@ -104,29 +114,26 @@ class CustomPolicy(Policy):
                 target = self.target_model.predict(state)  # q-values of the target model for each action
                 q_future = max(self.target_model.predict(next_state)[0])  # the max q-value possible from the next state
                 # print("*************** q values: " + str(target))
-                target[0][self.ACTIONS.index(action)] = reward + self.gamma * q_future  # the target q-value we want our model to achieve for the specific action
+                target[0][self.ACTIONS.index(
+                    action)] = reward + self.gamma * q_future  # the target q-value we want our model to achieve for the specific action
                 targets.append(target[0].tolist())
                 data.append(state[0].tolist())
-            self.model.fit(np.array(data), np.array(targets), epochs=1, verbose=0)  # training and updating the weights of the main model for the target activation.
+            self.model.fit(np.array(data), np.array(targets), epochs=1,
+                           verbose=0)  # training and updating the weights of the main model for the target activation.
 
-            if not round % 50:
-                # updating the weights of the target model in relation to the main model.
-                weights = self.model.get_weights()
-                target_weights = self.target_model.get_weights()
-                for i in range(len(target_weights)):
-                    target_weights[i] = weights[i] * self.t + target_weights[i] * (1 - self.t)
-                self.target_model.set_weights(target_weights)
-        # # # TODO: delete this  -- DOESNT WORK FOR NOW, list of arrays?
-        # if round > BATCH_THRESHOLD and not round % 500:
-        #     # print weights
-        #     # print(self.weights)
+            # if not round % 50:
+            # updating the weights of the target model in relation to the main model.
+            weights = self.model.get_weights()
+            target_weights = self.target_model.get_weights()
+            for i in range(len(target_weights)):
+                target_weights[i] = weights[i] * self.t + target_weights[i] * (1 - self.t)
+            self.target_model.set_weights(target_weights)
+
+        # # TODO: testing if the weights get too high
+        # if not round % 500 and round > 0:
         #     weights = self.model.get_weights()
-        #     for w_l in weights:
-        #         print("max weight: " + str(np.max(w_l)))
-        #         print("min weight: " + str(np.min(w_l)))
-        #         print("positives amount: " + str(len(w_l[w_l > 0])))
-        #         print("negatives amount: " + str(len(w_l[w_l <= 0])))
-        #         print("**********************")
+        #     for w in weights:
+        #         print(w)
 
     def act(self, round, prev_state, prev_action, reward, new_state, too_slow):
         """
@@ -162,7 +169,8 @@ class CustomPolicy(Policy):
         window = np.zeros((WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH))
         for i in range(-RADIUS, RADIUS + 1):
             for j in range(-RADIUS, RADIUS + 1):
-                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
+                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (
+                            head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
         return window.reshape((1, NUM_ELEMENTS))
 
     def __process_state_2(self, state):
@@ -175,7 +183,8 @@ class CustomPolicy(Policy):
         window = np.zeros((WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH))
         for i in range(-RADIUS, RADIUS + 1):
             for j in range(-RADIUS, RADIUS + 1):
-                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
+                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (
+                            head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
         return window.reshape((1, WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH, 1))
 
     def __process_state_3(self, state):
@@ -188,13 +197,16 @@ class CustomPolicy(Policy):
         window = np.zeros((WINDOW_SIDE_LENGTH, WINDOW_SIDE_LENGTH))
         for i in range(-RADIUS, RADIUS + 1):
             for j in range(-RADIUS, RADIUS + 1):
-                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
+                window[i + RADIUS, j + RADIUS] = board[(head_pos[0] + i + self.board_size[0]) % self.board_size[0], (
+                            head_pos[1] + j + self.board_size[1]) % self.board_size[1]]
         return to_categorical(window, num_classes=VALUES).reshape((1, NUM_ELEMENTS * VALUES))
 
     def __process_state_4(self, state):
         board, head_pos = state[0], state[1][0]
-        rows = [i % self.board_size[0] for i in range(head_pos[0] - RADIUS + self.board_size[0], head_pos[0] + RADIUS + self.board_size[0] + 1)]
-        cols = [i % self.board_size[1] for i in range(head_pos[1] - RADIUS + self.board_size[1], head_pos[1] + RADIUS + self.board_size[1] + 1)]
+        rows = [i % self.board_size[0] for i in
+                range(head_pos[0] - RADIUS + self.board_size[0], head_pos[0] + RADIUS + self.board_size[0] + 1)]
+        cols = [i % self.board_size[1] for i in
+                range(head_pos[1] - RADIUS + self.board_size[1], head_pos[1] + RADIUS + self.board_size[1] + 1)]
         window = board[rows, :][:, cols]
         # return to_categorical(window, num_classes=VALUES).reshape((1, NUM_ELEMENTS * VALUES))
         return window.reshape((1, NUM_ELEMENTS))
@@ -223,16 +235,16 @@ class CustomPolicy(Policy):
     def create_model_3(self):
         model = Sequential()
         model.add(Dense(256, activation='tanh', kernel_regularizer=l2()))
-        model.add(Dense(len(self.ACTIONS)))
+        model.add(Dense(len(self.ACTIONS), kernel_regularizer=l2()))
         model.compile(optimizer=Adam(lr=self.lr), loss='mean_squared_error')
         return model
 
     def create_model_4(self):
         model = Sequential()
-        model.add(Dense(512, activation='tanh'))
-        model.add(Dense(256, activation='tanh'))
-        model.add(Dense(128, activation='tanh'))
-        model.add(Dense(64, activation='tanh'))
+        model.add(Dense(512, activation='tanh', kernel_regularizer=l2()))
+        model.add(Dense(256, activation='tanh', kernel_regularizer=l2()))
+        model.add(Dense(128, activation='tanh', kernel_regularizer=l2()))
+        model.add(Dense(64, activation='tanh', kernel_regularizer=l2()))
         model.add(Dense(64, activation='tanh', kernel_regularizer=l2()))
         model.add(Dense(len(self.ACTIONS)))
         model.compile(optimizer=Adam(lr=self.lr), loss='mean_squared_error')
