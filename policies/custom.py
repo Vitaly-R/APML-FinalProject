622fcc7ffe3a45a9d5d012260b991828646af9a6
Vitaly-R <vitaly.rajev@gmail.com>
2020-03-05 12:32:29 +0200

    Ziv - moving to my pc

--- policies/custom.py
+++ policies/custom.py
@@ -15,11 +15,11 @@ MAX_BATCH_SIZE = 30
 MIN_BATCH_SIZE = 1
 # BATCH_THRESHOLD = 20 * BATCH_SIZE
 BATCH_THRESHOLD = 100
-RADIUS = 5
-GAMMA = 0.15
-INITIAL_EPSILON = 0.6
+RADIUS = 3
+GAMMA = 0.1
+INITIAL_EPSILON = 0.1
 # EPSILON_DECAY = 0.999
-EPSILON_DECAY_ROUND = BATCH_THRESHOLD
+# EPSILON_DECAY_ROUND = BATCH_THRESHOLD
 # EPSILON_MIN = 0.05
 WINDOW_SIDE_LENGTH = 2 * RADIUS + 1
 NUM_ELEMENTS = WINDOW_SIDE_LENGTH ** 2
@@ -72,9 +72,9 @@ class CustomPolicy(Policy):
             print('creating simple network')
             self.model = self.create_model_3()
             self.target_model = self.create_model_3()
-            self.model.predict(np.zeros((1, NUM_ELEMENTS)))
-            self.__process_state = self.__process_state_4
-        self.exploration_decay = self.epsilon / (self.game_duration - self.score_scope - EPSILON_DECAY_ROUND)
+            self.model.predict(np.zeros((1, NUM_ELEMENTS * VALUES)))
+            self.__process_state = self.__process_state_5
+        self.exploration_decay = self.epsilon / (self.game_duration - self.score_scope)
         self.memory = deque(maxlen=BATCH_THRESHOLD * 5)
         self.batch_size = BATCH_SIZE
         self.t = 0.5  # a learning rate for the weights of the target model in relation to the main model
@@ -95,39 +95,66 @@ class CustomPolicy(Policy):
                         policy for a few rounds in a row. you may use this to make your
                         computation time smaller (by lowering the batch size for example).
         """
-        if round >= EPSILON_DECAY_ROUND:
-            # self.epsilon = EPSILON_MIN if self.epsilon <= EPSILON_MIN else self.epsilon - self.exploration_decay
-            self.epsilon = self.epsilon - self.exploration_decay
+        if round < self.batch_size:
+            return
 
-        if round > BATCH_THRESHOLD:
-            # # In order for the model not to start training too early, we wait until enough states are saved.
-            # if too_slow:
-            #     self.batch_size = max(self.batch_size // 2, MIN_BATCH_SIZE)
-            # elif not round % 100:  # to prevent from happening too often
-            #     self.batch_size = min(self.batch_size + 1, MAX_BATCH_SIZE)
-            # # print("****************** batch size: " + str(self.batch_size))
+        self.epsilon = self.epsilon - self.exploration_decay
 
-            samples = r.sample(self.memory, self.batch_size)
-            data = list()
-            targets = list()
-            for (state, action, reward, next_state) in samples:
-                target = self.target_model.predict(state)  # q-values of the target model for each action
-                q_future = max(self.target_model.predict(next_state)[0])  # the max q-value possible from the next state
-                # print("*************** q values: " + str(target))
-                target[0][self.ACTIONS.index(
-                    action)] = reward + self.gamma * q_future  # the target q-value we want our model to achieve for the specific action
-                targets.append(target[0].tolist())
-                data.append(state[0].tolist())
-            self.model.fit(np.array(data), np.array(targets), epochs=1,
-                           verbose=0)  # training and updating the weights of the main model for the target activation.
+        samples = r.sample(self.memory, self.batch_size)
+        data = list()
+        targets = list()
+        for (state, action, reward, next_state) in samples:
+            target = self.target_model.predict(state)  # q-values of the target model for each action
+            q_future = max(self.target_model.predict(next_state)[0])  # the max q-value possible from the next state
+            # print("*************** q values: " + str(target))
+            target[0][self.ACTIONS.index(
+                action)] = reward + self.gamma * q_future  # the target q-value we want our model to achieve for the specific action
+            targets.append(target[0].tolist())
+            data.append(state[0].tolist())
+        self.model.fit(np.array(data), np.array(targets), epochs=1,
+                       verbose=0)  # training and updating the weights of the main model for the target activation.
 
-            # if not round % 50:
-            # updating the weights of the target model in relation to the main model.
-            weights = self.model.get_weights()
-            target_weights = self.target_model.get_weights()
-            for i in range(len(target_weights)):
-                target_weights[i] = weights[i] * self.t + target_weights[i] * (1 - self.t)
-            self.target_model.set_weights(target_weights)
+        # if not round % 50:
+        # updating the weights of the target model in relation to the main model.
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.t + target_weights[i] * (1 - self.t)
+        self.target_model.set_weights(target_weights)
+
+        # # if round >= EPSILON_DECAY_ROUND:
+        # #     # self.epsilon = EPSILON_MIN if self.epsilon <= EPSILON_MIN else self.epsilon - self.exploration_decay
+        # #
+        #
+        # if round > BATCH_THRESHOLD:
+        #     # # In order for the model not to start training too early, we wait until enough states are saved.
+        #     # if too_slow:
+        #     #     self.batch_size = max(self.batch_size // 2, MIN_BATCH_SIZE)
+        #     # elif not round % 100:  # to prevent from happening too often
+        #     #     self.batch_size = min(self.batch_size + 1, MAX_BATCH_SIZE)
+        #     # # print("****************** batch size: " + str(self.batch_size))
+        #
+        #     samples = r.sample(self.memory, self.batch_size)
+        #     data = list()
+        #     targets = list()
+        #     for (state, action, reward, next_state) in samples:
+        #         target = self.target_model.predict(state)  # q-values of the target model for each action
+        #         q_future = max(self.target_model.predict(next_state)[0])  # the max q-value possible from the next state
+        #         # print("*************** q values: " + str(target))
+        #         target[0][self.ACTIONS.index(
+        #             action)] = reward + self.gamma * q_future  # the target q-value we want our model to achieve for the specific action
+        #         targets.append(target[0].tolist())
+        #         data.append(state[0].tolist())
+        #     self.model.fit(np.array(data), np.array(targets), epochs=1,
+        #                    verbose=0)  # training and updating the weights of the main model for the target activation.
+        #
+        #     # if not round % 50:
+        #     # updating the weights of the target model in relation to the main model.
+        #     weights = self.model.get_weights()
+        #     target_weights = self.target_model.get_weights()
+        #     for i in range(len(target_weights)):
+        #         target_weights[i] = weights[i] * self.t + target_weights[i] * (1 - self.t)
+        #     self.target_model.set_weights(target_weights)
 
         # # TODO: testing if the weights get too high
         # if not round % 500 and round > 0:
@@ -208,8 +235,20 @@ class CustomPolicy(Policy):
         cols = [i % self.board_size[1] for i in
                 range(head_pos[1] - RADIUS + self.board_size[1], head_pos[1] + RADIUS + self.board_size[1] + 1)]
         window = board[rows, :][:, cols]
+        # return window[np.newaxis, ..., np.newaxis]
         # return to_categorical(window, num_classes=VALUES).reshape((1, NUM_ELEMENTS * VALUES))
         return window.reshape((1, NUM_ELEMENTS))
+
+    def __process_state_5(self, state):
+        board, head_pos = state[0], state[1][0]
+        rows = [i % self.board_size[0] for i in
+                range(head_pos[0] - RADIUS + self.board_size[0], head_pos[0] + RADIUS + self.board_size[0] + 1)]
+        cols = [i % self.board_size[1] for i in
+                range(head_pos[1] - RADIUS + self.board_size[1], head_pos[1] + RADIUS + self.board_size[1] + 1)]
+        window = board[rows, :][:, cols]
+        # return window[np.newaxis, ..., np.newaxis]
+        return to_categorical(window, num_classes=VALUES).reshape((1, NUM_ELEMENTS * VALUES))
+        # return window.reshape((1, NUM_ELEMENTS))
 
     def create_model_1(self):
         model = Sequential()
@@ -234,18 +273,20 @@ class CustomPolicy(Policy):
 
     def create_model_3(self):
         model = Sequential()
-        model.add(Dense(256, activation='tanh', kernel_regularizer=l2()))
+        model.add(Dense(128, activation='tanh', kernel_regularizer=l2()))
         model.add(Dense(len(self.ACTIONS), kernel_regularizer=l2()))
         model.compile(optimizer=Adam(lr=self.lr), loss='mean_squared_error')
         return model
 
     def create_model_4(self):
         model = Sequential()
-        model.add(Dense(512, activation='tanh', kernel_regularizer=l2()))
-        model.add(Dense(256, activation='tanh', kernel_regularizer=l2()))
-        model.add(Dense(128, activation='tanh', kernel_regularizer=l2()))
-        model.add(Dense(64, activation='tanh', kernel_regularizer=l2()))
-        model.add(Dense(64, activation='tanh', kernel_regularizer=l2()))
+        # model.add(Conv2D(32, 5, activation='relu'))
+        # model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(512, activation='relu'))
         model.add(Dense(len(self.ACTIONS)))
         model.compile(optimizer=Adam(lr=self.lr), loss='mean_squared_error')
         return model
